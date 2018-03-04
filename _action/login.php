9a9e87787a08bf495d7eb90422058d2fbaf7b589
Sebastian <32041785+Sisa49@users.noreply.github.com>
2018-03-04 19:37:10 +0100

    Create _actions

---
+++ _action/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sisa-
+ * Date: 04.03.2018
+ * Time: 19:35
+ */
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
-include '_lib/LockAbstract.php';
-include '_lib/Lock.php';
-include '_lib/SafeAbstract.php';
-include '_lib/MySafe.php';
-
-$myLock = new Lock();
-$mySafe = new MySafe($myLock);
-
-
-$safeModel = $mySafe->model;
-$producer =  $mySafe->getProducer();
-
-echo "Welcome to $safeModel produced by $producer <br />";
-
-$mySafe->unlock('1234');
-echo $mySafe->getSecret();
-$mySafe->lock();
+include '_layout/head.html';
+include '_layout/body_top.html';
+include '_action/route.php';
+include '_layout/body_bottom.html';
+//include '_lib/LockAbstract.php';
+//include '_lib/Lock.php';
+//include '_lib/SafeAbstract.php';
+//include '_lib/MySafe.php';
+//
+//$myLock = new Lock();
+//$mySafe = new MySafe($myLock);
+//
+//
+//$safeModel = $mySafe->model;
+//$producer =  $mySafe->getProducer();
+//
+//echo "Welcome to $safeModel produced by $producer <br />";
+//
+//$mySafe->unlock('1234');
+//echo $mySafe->getSecret();
+//$mySafe->lock();
 
