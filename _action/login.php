f400654141ae35cbf9036fd70db0c0e1c2cf98f2
Sebastian <32041785+Sisa49@users.noreply.github.com>
2018-03-05 20:29:21 +0100

    add login

--- _action/login.php
+++ _action/login.php
@@ -5,3 +5,15 @@
  * Date: 04.03.2018
  * Time: 19:35
  */
+$myLock = new Lock();
+$mySafe = new MySafe($myLock);
+
+
+$safeModel = $mySafe->model;
+$producer =  $mySafe->getProducer();
+
+echo "Welcome to $safeModel produced by $producer <br />";
+
+$mySafe->unlock('1234');
+echo $mySafe->getSecret();
+$mySafe->lock();
--- index.php
+++ index.php
@@ -13,16 +13,5 @@ include '_layout/body_bottom.html';
 //include '_lib/SafeAbstract.php';
 //include '_lib/MySafe.php';
 //
-//$myLock = new Lock();
-//$mySafe = new MySafe($myLock);
-//
-//
-//$safeModel = $mySafe->model;
-//$producer =  $mySafe->getProducer();
-//
-//echo "Welcome to $safeModel produced by $producer <br />";
-//
-//$mySafe->unlock('1234');
-//echo $mySafe->getSecret();
-//$mySafe->lock();
+
 
