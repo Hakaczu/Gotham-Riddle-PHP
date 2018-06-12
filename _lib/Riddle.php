01fdff237c695198d423d0426f2c7eb830d2f5b7
Sebastian Szypulski <sszypulski.kess@gmail.com>
2018-06-12 19:15:39 +0200

    Add Riddle Class

--- _lib/Riddle.php
+++ _lib/Riddle.php
@@ -8,5 +8,31 @@
 
 class Riddle
 {
+    private $riddle_id;
+    private $riddle_record;
+    private $db_connect;
+
+    public function __construct(DataBaseOperations $db_connect)
+    {
+        $this -> db_connect = $db_connect;
+        $this ->draw_riddle();
+        $this->getRiddleRecord();
+    }
+
+    private function drawRiddle(){
+        $this->riddle_id = rand(1,$this->db_connect->get_count_record('riddles'));
+    }
+
+    private function getRiddleRecord(){
+        $this->riddle_record=$this->db_connect->get_record('riddles', $this->riddle_id);
+    }
+
+    public function getRiddle(){
+        return $this->riddle_record['riddle'];
+    }
+
+    public function getAnswer(){
+        return $this->riddle_record['answer'];
+    }
 
 }
